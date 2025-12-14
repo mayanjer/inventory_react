755ad2936c2602c4a43a65bf9d1e4eee5637d4e9
mayanja <mayanjer3@gmail.com>
2025-12-14 10:46:06 +0300

    Added a form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,35 @@
-
+import { Col, Row } from "react-bootstrap";
 
 function Form() {
-    return (
-        <form action="">
-            <div className="card container">
+  return (
+    <form action="">
+      <div className="card container">
+        <Row>
+          <Col>
+            <h4>Name</h4>
             <div>
-                Name:&nbsp;
-            <input type="text" placeholder = "Name" />
-                </div><br/>
-                <div>
-                    Password:&nbsp;
-                    <input type="text" placeholder = "Password"/>
-                </div>
-
+              <input type="text" placeholder="Name" />
             </div>
+            <h4 className="mt-3">Password</h4>
 
-
-
-        </form>
-    )
+            <div>
+              <input type="text" placeholder="Password" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <button type="submit" className="mt-3">
+              Submit
+            </button>
+          </Col>
+          <Col>
+            <button className="mt-3 "> cancel</button>
+          </Col>
+        </Row>
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
