ca2f4350297d715c353cbba83a3763429f11f63e
mayanja <mayanjer3@gmail.com>
2025-12-16 10:32:38 +0300

    added a stable form from react bootstrap

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,30 @@
-import { Col, Row } from "react-bootstrap";
 
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
 
-function Form() {
-    function inputHandler(event) {
-        console.log(event.target.value)
-    }
+function SignUpForm() {
   return (
-    <form action="">
-      <div className="card container">
-        <Row>
-          <Col>
-            <h4>Name</h4>
-            <div>
-                          <input type="text" placeholder="Name" onChange={ inputHandler} />
-            </div>
-            <h4 className="mt-3">Password</h4>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-            <div>
-                          <input type="text" placeholder="Password" onChange={ inputHandler} />
-            </div>
-          </Col>
-        </Row>
-      </div>
-    </form>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
-export default Form;
+export default SignUpForm;
